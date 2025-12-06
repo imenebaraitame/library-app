35855354e4dd6664046e1d79b3dc9c7aef4f636e
imenebaraitame <baraitame.imene@gmail.com>
2025-12-06 18:02:15 +0100

    fix: Add middle middleware after routes to catch errors

--- src/app.js
+++ src/app.js
@@ -2,18 +2,16 @@ import express from 'express';
 import dotenv from "dotenv";
 import appRoutes from "./routes/index.js";
 import connectDB from "./config/dataBase.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 const app = express();
-// Load .env variables
-dotenv.config();
-app.use('/books',appRoutes);
 
-
+dotenv.config();// Load .env variables
 app.use(express.json());
-// connect to database
 connectDB();
 
 // routes
 app.use(appRoutes);
 
+app.use(errorMiddleware);
 export default app;
