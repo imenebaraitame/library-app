92e1babfc410b76bc56e2e9e51085d2e13ca4956
imenebaraitame <baraitame.imene@gmail.com>
2025-12-06 17:48:45 +0100

    Fix: improve error middleware with proper error handeling

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,16 +1,19 @@
 const errorMiddleware = (err, req, res, next) => { 
-    console.log("Error middleware called");
-    console.error('stack:', err.stack);
-    let status = err.status || 500;
-    
     if( err.name === 'ValidationError') {
-        status = 400;
-        message = " error from validation";
+        err.status = 400;
+        err.message = " Invalid data format. Please check your input.";
+    };
+    if( err.name === 'TypeError') {
+        err.status = 400;
+        err.message = " Type error occurred. check your input types.";
     }
-
-    res.status(300).json({
+    if (err.name === 'CastError') {
+        err.status= 404;
+        err.message= " Resource not found. Invalid ID format.";
+    }
+    res.status(err.status || 500).json({
+        success: false,
         message: err.message || 'Internal Server Error',
-        status: status
     });
 }
 
