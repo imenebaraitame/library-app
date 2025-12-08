bac99bf44b89a3f5e449ca55e293816a4f122862
imenebaraitame <baraitame.imene@gmail.com>
2025-12-08 18:22:44 +0100

    Add cart route to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import appRoutes from './booksRouter.js';
 import categoriesRoutes from './categoriesRouter.js';
+import cartRoutes from './cartRouter.js';
 
 const router = express.Router();
 
 router.use('/api/books', appRoutes);//http://localhost:5000/api/books/
 router.use('/api/categories',categoriesRoutes);//http://localhost:5000/api/categories/
+router.use('/api/cart', cartRoutes); // http://localhost:5000/api/cart/
 
 export default router
